4895c15f6e6becfed7bc4c7b79b4e18b9b7b2ae5
chnoumanejaz <noumanejaz92@gmail.com>
2024-03-28 13:00:05 +0500

    image and resume  update

--- src/constants/images.js
+++ src/constants/images.js
@@ -24,6 +24,8 @@ import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
 
 import profile from '../assets/profile.png';
+import profile2 from '../assets/profile2.png';
+import profile3 from '../assets/profile3.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 
@@ -60,6 +62,8 @@ export default {
   about03,
   about04,
   profile,
+  profile2,
+  profile3,
   circle,
   logo,
   adidas,
--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -44,7 +44,7 @@ function Header() {
         whileInView={{ opacity: [0, 1] }}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__header-img">
-        <img src={images.profile} alt="profile_Nouman" />
+        <img src={images.profile3} alt="profile_Nouman" />
         <motion.img
           whileInView={{ scale: [0, 1] }}
           transition={{ duration: 1, ease: 'easeInOut' }}
